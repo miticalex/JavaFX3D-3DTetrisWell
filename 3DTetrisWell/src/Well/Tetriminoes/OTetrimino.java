98a10eb13decebb8c3b315ecf81358f84f887dc4
Alexander Mitic <miticalexander@yahoo.com>
2018-07-14 22:21:13 +0200

    Otetrimino positioning changed

--- 3DTetrisWell/src/Well/Tetriminoes/OTetrimino.java
+++ 3DTetrisWell/src/Well/Tetriminoes/OTetrimino.java
@@ -8,10 +8,10 @@ public class OTetrimino extends Tetrimino {
     public OTetrimino(double fieldSize) {
         super(fieldSize);
        
-        addBox(-1, -1, 0);
-        addBox(0, -1, 0);
-        addBox(-1, 0, 0);
         addBox(0, 0, 0);
+        addBox(1, 0, 0);
+        addBox(0, 1, 0);
+        addBox(1, 1, 0);
     }
     public OTetrimino() {
         this(Well.Well.FIELD_SIZE);
