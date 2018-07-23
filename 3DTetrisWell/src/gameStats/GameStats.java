e1cc6df0b5b2a890fbda499b37a0f004052b7483
Alexander Mitic <miticalexander@yahoo.com>
2018-07-23 03:34:50 +0200

    GameStats section appearance improved

--- 3DTetrisWell/src/gameStats/GameStats.java
+++ 3DTetrisWell/src/gameStats/GameStats.java
@@ -71,19 +71,6 @@ public class GameStats extends Group implements Updateable {
         setLabels();
         setInitialStats();
         setRotationButtons();
-        
-        Text pauseLabel = new Text("Pause Button:      PAUSE/BREAK");
-        Text newGameLabel = new Text("Start New Game: ESCAPE");
-        pauseLabel.setTranslateX(width/6);
-        pauseLabel.setTranslateY(680);
-        pauseLabel.setFont(Font.font(20));
-        pauseLabel.setFill(Color.YELLOW);
-        newGameLabel.setTranslateX(width/6);
-        newGameLabel.setTranslateY(720);
-        newGameLabel.setFont(Font.font(20));
-        newGameLabel.setFill(Color.YELLOW);
-        
-        this.getChildren().addAll(pauseLabel, newGameLabel);
     }
     
     private void setBackGround(){
@@ -91,7 +78,7 @@ public class GameStats extends Group implements Updateable {
         
         gameName.setFont(new Font(50));
         gameName.setTranslateX(width/2 - gameName.getBoundsInParent().getWidth()/2);
-        gameName.setTranslateY(100);
+        gameName.setTranslateY(70);
         gameName.setFill(Color.TURQUOISE);
         this.getChildren().addAll(background, gameName);
     }
@@ -99,7 +86,7 @@ public class GameStats extends Group implements Updateable {
     private void setLabels() {
         labelsVBox = new VBox(dimensionsLabel, timeLabel, levelLabel, 
                 pointsLabel, linesClearedLabel, blocksClearedLabel);
-        labelsVBox.setTranslateY(150);
+        labelsVBox.setTranslateY(100);
         labelsVBox.setTranslateX(width/6);
         
         for (Node node : labelsVBox.getChildren()) {
@@ -109,12 +96,24 @@ public class GameStats extends Group implements Updateable {
         }
         
         this.getChildren().addAll(labelsVBox);
+        
+        Text pauseLabel = new Text("Pause Button:      PAUSE/BREAK");
+        Text newGameLabel = new Text("Start New Game: ESCAPE");
+        pauseLabel.setTranslateX(width/6);
+        pauseLabel.setTranslateY(630);
+        pauseLabel.setFont(Font.font(20));
+        pauseLabel.setFill(Color.YELLOW);
+        newGameLabel.setTranslateX(width/6);
+        newGameLabel.setTranslateY(670);
+        newGameLabel.setFont(Font.font(20));
+        newGameLabel.setFill(Color.YELLOW);
+        this.getChildren().addAll(pauseLabel, newGameLabel);
     }
     
     private void setInitialStats() {
         statsVBox = new VBox(dimensions, time, level, 
                 points, linesCleared, blocksCleared);
-        statsVBox.setTranslateY(150);
+        statsVBox.setTranslateY(100);
         statsVBox.setTranslateX(width/2);
         
         for (Node node : statsVBox.getChildren()) {
@@ -129,68 +128,68 @@ public class GameStats extends Group implements Updateable {
     private void setRotationButtons() {
         Text rotationButtonsLabel = new Text("Rotation Buttons:");
         rotationButtonsLabel.setTranslateX(width/6);
-        rotationButtonsLabel.setTranslateY(450);
+        rotationButtonsLabel.setTranslateY(400);
         rotationButtonsLabel.setFont(Font.font(20));
         rotationButtonsLabel.setFill(Color.YELLOW);
         
-        Rectangle positiveZ = new Rectangle(width/12, 470, width/8, width/8);
+        Rectangle positiveZ = new Rectangle(width/12, 420, width/8, width/8);
         positiveZ.setFill(new ImagePattern(new Image("resources/positiveZ.png")));
         positiveZ.setOnMouseClicked(e-> {well.rotateFallingTetrimino(Rotate.Z_AXIS, +90);});
         
-        Rectangle negativeZ = new Rectangle(width/12, 570, width/8, width/8);
+        Rectangle negativeZ = new Rectangle(width/12, 520, width/8, width/8);
         negativeZ.setFill(new ImagePattern(new Image("resources/negativeZ.png")));
         negativeZ.setOnMouseClicked(e-> {well.rotateFallingTetrimino(Rotate.Z_AXIS, -90);});
         
-        Rectangle positiveY = new Rectangle(3*width/12, 470, width/8, width/8);
+        Rectangle positiveY = new Rectangle(3*width/12, 420, width/8, width/8);
         positiveY.setFill(new ImagePattern(new Image("resources/positiveY.png")));
         positiveY.setOnMouseClicked(e-> {well.rotateFallingTetrimino(Rotate.Y_AXIS, +90);});
         
-        Rectangle negativeY = new Rectangle(3*width/12, 570, width/8, width/8);
+        Rectangle negativeY = new Rectangle(3*width/12, 520, width/8, width/8);
         negativeY.setFill(new ImagePattern(new Image("resources/negativeY.png")));
         negativeY.setOnMouseClicked(e-> {well.rotateFallingTetrimino(Rotate.Y_AXIS, -90);});
         
-        Rectangle positiveX = new Rectangle(5*width/12, 470, width/8, width/8);
+        Rectangle positiveX = new Rectangle(5*width/12, 420, width/8, width/8);
         positiveX.setFill(new ImagePattern(new Image("resources/positiveX.png")));
         positiveX.setOnMouseClicked(e-> {well.rotateFallingTetrimino(Rotate.X_AXIS, +90);});
         
-        Rectangle negativeX = new Rectangle(5*width/12, 570, width/8, width/8);
+        Rectangle negativeX = new Rectangle(5*width/12, 520, width/8, width/8);
         negativeX.setFill(new ImagePattern(new Image("resources/negativeX.png")));
         negativeX.setOnMouseClicked(e-> {well.rotateFallingTetrimino(Rotate.X_AXIS, -90);});
         
         
-        Text alternativeRotationButtonsLabel = new Text("Alternative buttons:");
+        Text alternativeRotationButtonsLabel = new Text("Alternative controls:");
         alternativeRotationButtonsLabel.setTranslateX(0.62*width);
-        alternativeRotationButtonsLabel.setTranslateY(450);
+        alternativeRotationButtonsLabel.setTranslateY(400);
         alternativeRotationButtonsLabel.setFont(Font.font(20));
         alternativeRotationButtonsLabel.setFill(Color.YELLOW);
         
         Text positiveZLabel = new Text("INS \\\n U");
-        positiveZLabel.setTranslateX(7*width/12);positiveZLabel.setTranslateY(485);
+        positiveZLabel.setTranslateX(7*width/12);positiveZLabel.setTranslateY(435);
         positiveZLabel.setFont(Font.font(15));
         positiveZLabel.setFill(Color.YELLOW);
         
         Text negativeZLabel = new Text("DEL \\\n J");
-        negativeZLabel.setTranslateX(7*width/12);negativeZLabel.setTranslateY(585);
+        negativeZLabel.setTranslateX(7*width/12);negativeZLabel.setTranslateY(535);
         negativeZLabel.setFont(Font.font(15));
         negativeZLabel.setFill(Color.YELLOW);
         
         Text positiveYLabel = new Text("HOME \\\n I");
-        positiveYLabel.setTranslateX(9*width/12-15);positiveYLabel.setTranslateY(485);
+        positiveYLabel.setTranslateX(9*width/12-15);positiveYLabel.setTranslateY(435);
         positiveYLabel.setFont(Font.font(15));
         positiveYLabel.setFill(Color.YELLOW);
         
         Text negativeYLabel = new Text("END \\\n K");
-        negativeYLabel.setTranslateX(9*width/12-15);negativeYLabel.setTranslateY(585);
+        negativeYLabel.setTranslateX(9*width/12-15);negativeYLabel.setTranslateY(535);
         negativeYLabel.setFont(Font.font(15));
         negativeYLabel.setFill(Color.YELLOW);
         
         Text positiveXLabel = new Text("PGUP \\\n O");
-        positiveXLabel.setTranslateX(11*width/12-15);positiveXLabel.setTranslateY(485);
+        positiveXLabel.setTranslateX(11*width/12-15);positiveXLabel.setTranslateY(435);
         positiveXLabel.setFont(Font.font(15));
         positiveXLabel.setFill(Color.YELLOW);
         
         Text negativeXLabel = new Text("PGDN \\\n L");
-        negativeXLabel.setTranslateX(11*width/12-15);negativeXLabel.setTranslateY(585);
+        negativeXLabel.setTranslateX(11*width/12-15);negativeXLabel.setTranslateY(535);
         negativeXLabel.setFont(Font.font(15));
         negativeXLabel.setFill(Color.YELLOW);
         
