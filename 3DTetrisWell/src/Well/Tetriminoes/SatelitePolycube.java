23aacd913932f1391f9bba17c285e074d20b9c65
Alexander Mitic <miticalexander@yahoo.com>
2018-08-04 18:54:10 +0200

    Improved/refurbished SatelitePolyCube

--- 3DTetrisWell/src/Well/Tetriminoes/SatelitePolycube.java
+++ 3DTetrisWell/src/Well/Tetriminoes/SatelitePolycube.java
@@ -8,16 +8,31 @@ public class SatelitePolycube extends Tetrimino {
     public SatelitePolycube(double fieldSize) {
         super(fieldSize);
        
+        addBox(0, 0, 0);
+        addBox(0, 0, -1);
+        addBox(0, 0, +1);
         addBox(0, -1, 0);
         addBox(-1, 0, 0);
         addBox(+1, 0, 0);
         addBox(0, +1, 0);
-        addBox(0, 0, 0);
-        addBox(0, 0, -1);
-        addBox(0, 0, +1);
+        
+        add2DAppearance();
     }
     
     public SatelitePolycube() {
         this(Well.Well.FIELD_SIZE);
     }
+    
+    
+    private void add2DAppearance() {
+        tetrimino2D = new Tetrimino2D();
+        
+        addSquare(0, -1, 0);
+        addSquare(-1, 0, 0);
+        addSquare(+1, 0, 0);
+        addSquare(0, +1, 0);
+        addSquare(0, 0, 0);
+        addSquare(0, 0, -1);
+        addSquare(0, 0, +1);
+    }   
 }
