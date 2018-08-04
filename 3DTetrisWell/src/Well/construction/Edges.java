32224921d9c4b9716ce937c7450e5ff4d3cb39c6
Alexander Mitic <miticalexander@yahoo.com>
2018-08-05 01:12:38 +0200

    Improved Edge appearance
    - Solved overpalling between the edges, and between the edges and the walls

--- 3DTetrisWell/src/Well/construction/Edges.java
+++ 3DTetrisWell/src/Well/construction/Edges.java
@@ -24,24 +24,28 @@ public class Edges extends Group{
         for (int i = -1; i < 2*width+1; i++) {
             Box rearEdge = new Box(fieldSize/2, fieldSize/2, edgeThickness);
             rearEdge.setTranslateX(fieldSize*(-0.5*width + 0.25 + 0.5*i));
-            rearEdge.setTranslateY(fieldSize*(-0.5*height - 0.24)); // addNodeToGroupGridXYZ(edges, rearEdge, 0.5*i - 0.25, -0.75, -0.5);
+            rearEdge.setTranslateY(fieldSize*(-0.5*height - 0.25)); // addNodeToGroupGridXYZ(edges, rearEdge, 0.5*i - 0.25, -0.75, -0.5);
+            rearEdge.setTranslateZ(-edgeThickness/2);
             
             Box frontEdge = new Box(fieldSize/2, fieldSize/2, edgeThickness);
             frontEdge.setTranslateX(fieldSize*(-0.5*width + 0.25 + 0.5*i));
-            frontEdge.setTranslateY(fieldSize*(0.5*height + 0.24)); // addNodeToGroupGridXYZ(edges, frontEdge, 0.5*i - 0.25, height - 0.25, -0.5);
+            frontEdge.setTranslateY(fieldSize*(0.5*height + 0.25)); // addNodeToGroupGridXYZ(edges, frontEdge, 0.5*i - 0.25, height - 0.25, -0.5);
+            frontEdge.setTranslateZ(-edgeThickness/2);
             
             this.getChildren().addAll(rearEdge, frontEdge);
         }
         
         for (int i = 0; i < 2*height; i++) {
             Box leftEdge = new Box(fieldSize/2, fieldSize/2, edgeThickness);
-            leftEdge.setTranslateX(fieldSize*(-0.5*width - 0.24));
+            leftEdge.setTranslateX(fieldSize*(-0.5*width - 0.25));
             leftEdge.setTranslateY(fieldSize*(-0.5*height + 0.25 + 0.5*i)); // addNodeToGroupGridXYZ(edges, leftEdge, -0.75, 0.5*i - 0.25, -0.5);
+            leftEdge.setTranslateZ(-edgeThickness/2);
             
             Box rightEdge = new Box(fieldSize/2, fieldSize/2, edgeThickness);
-            rightEdge.setTranslateX(fieldSize*(0.5*width + 0.24));
+            rightEdge.setTranslateX(fieldSize*(0.5*width + 0.25));
             rightEdge.setTranslateY(fieldSize*(-0.5*height + 0.25 + 0.5*i)); //addNodeToGroupGridXYZ(edges, rightEdge, width - 0.25, 0.5*i - 0.25, -0.5);
-        
+            rightEdge.setTranslateZ(-edgeThickness/2);
+            
             this.getChildren().addAll(leftEdge, rightEdge);
         }
     }
