9485eb2890febe29c89b36d5b4c3b3cf34258d75
Alexander Mitic <miticalexander@yahoo.com>
2018-08-04 18:47:32 +0200

    Improved/refurbished TargetPolyCube

--- 3DTetrisWell/src/Well/Tetriminoes/TargetPolycube.java
+++ 3DTetrisWell/src/Well/Tetriminoes/TargetPolycube.java
@@ -8,15 +8,27 @@ public class TargetPolycube extends Tetrimino {
     public TargetPolycube(double fieldSize) {
         super(fieldSize);
        
+        addBox(0, 0, -1);
+        addBox(0, 0, 0);
         addBox(0, -1, 0);
         addBox(-1, 0, 0);
         addBox(+1, 0, 0);
         addBox(0, +1, 0);
-        addBox(0, 0, 0);
-        addBox(0, 0, -1);
+        
+        add2DAppearance();
     }
     
     public TargetPolycube() {
         this(Well.Well.FIELD_SIZE);
     }
+    private void add2DAppearance() {
+        tetrimino2D = new Tetrimino2D();
+        
+        addSquare(0, -1, 0);
+        addSquare(-1, 0, 0);
+        addSquare(+1, 0, 0);
+        addSquare(0, +1, 0);
+        addSquare(0, 0, 0);
+        addSquare(0, 0, -1);
+    }   
 }
