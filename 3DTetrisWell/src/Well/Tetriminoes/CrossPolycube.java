b7ccc32813f370d7b36bc8eaef360a5fd7713c2b
Alexander Mitic <miticalexander@yahoo.com>
2018-08-04 19:06:04 +0200

    Improved/refurbished Cross and JCrossPolycube

--- 3DTetrisWell/src/Well/Tetriminoes/CrossPolycube.java
+++ 3DTetrisWell/src/Well/Tetriminoes/CrossPolycube.java
@@ -8,11 +8,17 @@ public class CrossPolycube extends Tetrimino {
     public CrossPolycube(double fieldSize) {
         super(fieldSize);
        
+        addBox(0, 0, 0);
         addBox(0, -1, 0);
         addBox(-1, 0, 0);
         addBox(+1, 0, 0);
         addBox(0, +1, 0);
-        addBox(0, 0, 0);
+        
+//        addBox(0, -1, 0);
+//        addBox(-1, 0, 0);
+//        addBox(+1, 0, 0);
+//        addBox(0, +1, 0);
+//        addBox(0, 0, 0);
     }
     
     public CrossPolycube() {
--- 3DTetrisWell/src/Well/Tetriminoes/JCrossPolycube.java
+++ 3DTetrisWell/src/Well/Tetriminoes/JCrossPolycube.java
@@ -8,15 +8,35 @@ public class JCrossPolycube extends Tetrimino {
     public JCrossPolycube(double fieldSize) {
         super(fieldSize);
        
+        addBox(0, 0, 0);
         addBox(0, -1, 0);
         addBox(-1, 0, 0);
+        addBox(+1, 0, -1);
         addBox(+1, 0, 0);
         addBox(0, +1, 0);
-        addBox(0, 0, 0);
-        addBox(+1, 0, -1);
+        
+//        addBox(0, -1, 0);
+//        addBox(-1, 0, 0);
+//        addBox(+1, 0, -1);
+//        addBox(+1, 0, 0);
+//        addBox(0, +1, 0);
+//        addBox(0, 0, 0);
+
+        add2DAppearance();
     }
     
     public JCrossPolycube() {
         this(Well.Well.FIELD_SIZE);
+    }
+    
+    private void add2DAppearance() {
+        tetrimino2D = new Tetrimino2D();
+        
+        addSquare(0, -1, 0);
+        addSquare(-1, 0, 0);
+        addSquare(+1, 0, 0);
+        addSquare(0, +1, 0);
+        addSquare(0, 0, 0);
+        addSquare(+1, 0, -1);
     }
 }
